cfeee2eac4ab7e8f2323ce97865f67384a2248be
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-03-22 11:04:44 -0400

    done with task 6

--- index.js
+++ index.js
@@ -274,18 +274,26 @@ Use addArtist to do the following:
 2. Add this object of information to the end of the array
     { 
       id: 20,
-      name: Your Name Here, 
-      years: Your Birth Year - current day,
+      name: "Jacob SHomali", 
+      years: "2000 - march 22nd 2021",
       genre: Web Design, 
-      nationality: Your Nationality Here
-      bio: Add 1-2 sentences (or use lorem ipsum)
+      nationality: Palestinian
+      bio: like peanut butter and manga.
     }  
 3. Return the resulting array
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
+function addArtist(artists){
+    artist.push({ 
+      id: 20,
+      name: "Jacob SHomali", 
+      years: "2000 - march 22nd 2021",
+      genre: "Web Design", 
+      nationality: "Palestinian",
+      bio: "like peanut butter and manga"
+    })
+    return artists;
   }
 
   
